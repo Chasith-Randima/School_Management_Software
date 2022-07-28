81ce33ef3a3cd89cb4cc385268dd008d61581460
Chasith-Randima <chasith19@gmail.com>
2022-07-28 17:45:11 +0530

    delete screen designed

--- AllStudents.cs
+++ AllStudents.cs
@@ -40,7 +40,7 @@ int nLeftRect, // x-coordinate of upper-left corner
 int nTopRect, // y-coordinate of upper-left corner
 int nRightRect, // x-coordinate of lower-right corner
 int nBottomRect, // y-coordinate of lower-right corner
-int nWidthEllipse, // height of ellipse
+int nWidthEllipse, // height of ellipse 
 int nHeightEllipse // width of ellipse
 );
 
