629f045ed3f89e119b9e60e30e27fc9c213a622a
Chasith-Randima <chasith19@gmail.com>
2022-07-31 17:54:14 +0530

    delete one page backend functionality developed

--- DeleteOne.cs
+++ DeleteOne.cs
@@ -41,7 +41,7 @@ int nTopRect, // y-coordinate of upper-left corner
 int nRightRect, // x-coordinate of lower-right corner
 int nBottomRect, // y-coordinate of lower-right corner
 int nWidthEllipse, // height of ellipse
-int nHeightEllipse // width of ellipse
+int nHeightEllipse // width of ellipse 
 );
 
 
